5b531b7057e03eb79294086774a89a3e6a4bde35
Jorge MR <jorge_mr@icloud.com>
2017-09-12 23:24:55 -0500

    First Commit, using just Alamofire to connect with a wordpress api v2

--- AlamofireSwiftyJson/ViewController.swift
+++ AlamofireSwiftyJson/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://blog.lascomidasdecu.com/wp-json/wp/v2/posts/14").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
